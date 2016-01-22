0d14d3704df51e3a73a6de976a6685a3a41926ee
Brad Pizzimenti <brad@gennakersystems.com>
2016-01-22 09:56:26 -0800

    add fifth spec ✖(red)

--- spec/specs.js
+++ spec/specs.js
@@ -11,4 +11,7 @@ describe('pingPong', function () {
   it("displays 'pong' for any number divisible by 5", function () {
     expect(pingPong(5)).to.eql([1,2,'ping',4,'pong']);
   });
+  it("displays 'pingpong' for any number divisible by 15", function () {
+    expect(pingPong(15)).to.eql([1,2,'ping',4,'pong','ping',7,8,'ping','pong',11,'ping',14,'pingpong']);
+  })
 });
