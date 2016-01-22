1f350899fe3baabeab9694daf0ed7a6b7aa9433a
Brad Pizzimenti <brad@gennakersystems.com>
2016-01-22 09:13:09 -0800

    update specs to 'deeply' equal array equivalency

--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,8 @@
 describe('pingPong', function () {
   it("displays 1 for any given valid input", function() {
-    expect(pingPong(1)).to.equal(1);
+    expect(pingPong(1)).to.eql([1]);
   });
   it("displays range up to 'rangeMax' value", function () {
-    expect(pingPong(2)).to.equal([1,2]);
+    expect(pingPong(2)).to.eql([1,2]);
   });
 });
