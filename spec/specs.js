ae0eaabafe05aa6e389a90365b123058826a680b
Brad Pizzimenti <brad@gennakersystems.com>
2016-01-22 10:06:29 -0800

    fix error in fifth spec

--- spec/specs.js
+++ spec/specs.js
@@ -12,6 +12,6 @@ describe('pingPong', function () {
     expect(pingPong(5)).to.eql([1,2,'ping',4,'pong']);
   });
   it("displays 'pingpong' for any number divisible by 15", function () {
-    expect(pingPong(15)).to.eql([1,2,'ping',4,'pong','ping',7,8,'ping','pong',11,'ping',14,'pingpong']);
+    expect(pingPong(15)).to.eql([1,2,'ping',4,'pong','ping',7,8,'ping','pong',11,'ping',13,14,'pingpong']);
   })
 });
