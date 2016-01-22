cae66103522485010262bacf7400aea5602e5f3c
Brad Pizzimenti <brad@gennakersystems.com>
2016-01-22 09:34:50 -0800

    complete third spec (green)

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 var pingPong = function(rangeMax) {
   var ppArray = [];
   for (var i = 0; i <= rangeMax; i++) {
+
+    if (i % 3 === 0) {
+      ppArray.push("ping");
+    } else {
+
     ppArray.push(i);
+    }
   }
   ppArray.splice(0,1);
   return ppArray;
