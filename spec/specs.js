2118c1edd417f0827f60aa4a1562d294d44a8c30
Brad Pizzimenti <brad@gennakersystems.com>
2016-01-22 09:01:19 -0800

    add second spec (red)

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,8 @@
 describe('pingPong', function () {
   it("displays 1 for any given valid input", function() {
     expect(pingPong(1)).to.equal(1);
-  })
+  });
+  it("displays range up to 'rangeMax' value", function () {
+    expect(pingPong(2)).to.equal([1,2]);
+  });
 });
