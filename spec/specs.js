81fdd87bbae0844da6c00baa9886704a1e75e2cf
Brad Pizzimenti <brad@gennakersystems.com>
2016-01-22 09:33:21 -0800

    add third spec (red)

--- spec/specs.js
+++ spec/specs.js
@@ -5,4 +5,7 @@ describe('pingPong', function () {
   it("displays range up to 'rangeMax' value", function () {
     expect(pingPong(2)).to.eql([1,2]);
   });
+  it("displays 'ping' for any number divisible by 3", function () {
+    expect(pingPong(3)).to.eql([1,2,'ping']);
+  });
 });
