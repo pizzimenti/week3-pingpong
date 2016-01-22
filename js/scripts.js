e353a925eb859a54b35887efdf6be0a84e650574
Brad Pizzimenti <brad@gennakersystems.com>
2016-01-22 09:14:15 -0800

    complete second spec (green)

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 var pingPong = function(rangeMax) {
-  return 1;
+  var ppArray = [];
+  for (var i = 0; i <= rangeMax; i++) {
+    ppArray.push(i);
+  }
+  ppArray.splice(0,1);
+  return ppArray;
 };
