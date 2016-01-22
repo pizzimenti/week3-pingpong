a984c81668a50710d149462b2d1f3b6a3e320339
Brad Pizzimenti <brad@gennakersystems.com>
2016-01-22 09:43:42 -0800

    complete fourth spec (green)

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,17 @@
 var pingPong = function(rangeMax) {
   var ppArray = [];
+
   for (var i = 0; i <= rangeMax; i++) {
 
     if (i % 3 === 0) {
       ppArray.push("ping");
+    } else if (i % 5 === 0) {
+      ppArray.push("pong");
     } else {
-
     ppArray.push(i);
     }
   }
+
   ppArray.splice(0,1);
   return ppArray;
 };
